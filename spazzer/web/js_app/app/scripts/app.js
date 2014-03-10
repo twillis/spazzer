a46934c5da119d8323f35f6443ac8142dc6264f9
Tom Willis <twillis@topgunhq.com>
2014-03-10 08:56:46 -0400

    swith to ui router

--- spazzer/web/js_app/app/scripts/app.js
+++ spazzer/web/js_app/app/scripts/app.js
@@ -1,18 +1,22 @@
 'use strict';
 
 angular.module('spazzerApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ui.router'
+]).config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider.state('main',
+                         {url: '/',
+                          templateUrl: 'views/main.html',
+                          controller: 'MainCtrl'});
+    $stateProvider.state('main.list',
+                         {url:'list/:start',
+                         templateUrl: 'views/list.html',
+                         controller: 'ListCtrl'});
+    $stateProvider.state('main.list.artist',
+                        {url:'/:artist',
+                        templateUrl: 'views/artist.html',
+                        controller: 'ArtistCtrl'});
+}]);
--- spazzer/web/js_app/app/scripts/controllers/main.js
+++ spazzer/web/js_app/app/scripts/controllers/main.js
@@ -1,25 +1,53 @@
 'use strict';
 
 angular.module('spazzerApp')
-  .controller('MainCtrl', ["$scope","$http", function ($scope, $http) {
-      $scope.items = [];
+    .controller('MainCtrl', ["$scope","$http", "$state", function ($scope, $http, $state) {
+        $scope.items = [];
 
-      $scope.showSet = function(idx){
-          console.log(idx);
-          if(idx == "all"){
-              var params = {start: ""};
-          }else{
-              var params = {start: idx};
-          }
-          $http.get("/api/collection/data", {params: params}).then(function(results){
-              $scope.items = results.data.items;
-              $scope.selectedArtistAlbums = null;
-          });
-      };
+        $scope.showSet = function(idx){
+            $state.go('main.list', {start:idx});
+        };
 
-      $scope.selectArtist = function(item){
-          $http.get(item.detail_url).then(function(results){
-             $scope.selectedArtistAlbums = results.data.items;
-          });
-      };
-  }]);
+    }])
+    .controller('ListCtrl',['$scope', '$http', '$state', '$stateParams', function($scope, $http, $state, $stateParams){
+        console.log($stateParams);
+        var idx = $stateParams.start;
+        if(idx == "all"){
+            var params = {start: ""};
+        }else{
+            var params = {start: idx};
+        }
+
+        $http.get("/api/collection/data", {params: params}).then(function(results){
+            $scope.items = results.data.items;
+            $scope.selectedArtistAlbums = null;
+        });
+
+        $scope.selectArtist = function(item){
+            $scope.selectedArtist = item;
+            $state.go('main.list.artist', {artist: item.name});
+        };
+
+        $scope.initArtist = function(name){
+            if($scope.items && name){
+                $scope.selectedArtist = _.find($scope.items, 
+                                           function(item){
+                                               return item.name == name;
+                                           });
+            }
+        };
+    }])
+    .controller('ArtistCtrl', ['$scope', '$http', '$state', '$stateParams', function($scope, $http, $state, $stateParams){
+        $scope.$watch('selectedArtist', function(oldv, newv){
+            var val = newv || oldv;
+            if(val){
+                $http.get(val.detail_url).then(function(results){
+                    $scope.selectedArtistAlbums = results.data.items;
+                });
+            }
+        });
+
+        $scope.$watch('items', function(oldv, newv){
+            $scope.initArtist($stateParams.artist);
+        });
+    }]);
