abc6fd995d92b2d83893daf93ea62084a69461d5
Tom Willis <twillis@topgunhq.com>
2014-03-15 09:01:21 -0400

    straightened out the funky state transitions and controller cross talk. cleaner? who know.

--- spazzer/web/js_app/app/scripts/controllers/main.js
+++ spazzer/web/js_app/app/scripts/controllers/main.js
@@ -1,61 +1,64 @@
 'use strict';
 
 angular.module('spazzerApp')
-    .controller('MainCtrl', ['$scope','$http', '$state', '$stateParams', function ($scope, $http, $state, $stateParams) {
-        $scope.items = [];
-        $scope.start = $stateParams.start;
+    .factory('MusicService', ['$http', function($http){
+        /*singleton style service, the "api" is that each of these methods will return promises*/
+        this.getCollection = function(start){
+            var params = {};
+
+            if(start === 'all'){
+                params = {start: ''};
+            }else{
+                params = {start: start};
+            }
+
+            return $http.get('/api/collection/data', {params: params});
+        };
+
+        this.getArtistDetail = function(artist){
+            return $http.get('/api/collection/detail', {params: {artist: artist}});
+        };
+
+        return this;
+    }])
+    .controller('MainCtrl', ['$scope','$state', function ($scope, $state) {
+        $scope.start = $state.params.start;
+
+        $scope.$root.$on('$stateChangeSuccess', function(event, tostate, toparams){
+            $scope.start = toparams.start;
+        });//needed because parent view is initialized once where children are created at state transition??
+
 
         $scope.showSet = function(idx){
             $state.go('main.list', {start:idx});
-          };
-
-        $scope.initStart = function(start){
-            $scope.start = start;
+            $scope.start = idx;
         };
 
-      }])
-    .controller('ListCtrl',['$scope', '$http', '$state', '$stateParams', function($scope, $http, $state, $stateParams){
-        var idx = $stateParams.start;
-        var params = null;
-        $scope.initStart(idx);
-
-        if(idx === 'all'){
-          params = {start: ''};
-        }else{
-          params = {start: idx};
-        }
-
-        $http.get('/api/collection/data', {params: params}).then(function(results){
+        $scope.isSelected = function(idx){
+            return $scope.start && $scope.start === idx;
+        };
+    }])
+    .controller('ListCtrl',['$scope', 'MusicService', '$state', '$stateParams', function($scope, MusicService, $state){
+        MusicService.getCollection($state.params.start).then(function(results){
             $scope.items = results.data.items;
-            $scope.selectedArtistAlbums = null;
-          });
+            if($scope.items && $state.params.artist){
+                $scope.selectedArtist = _.find($scope.items,
+                                               function(item){
+                                                   return item.name === $state.params.artist;
+                                               });
+            }else{
+                $scope.selectedArtist = null;
+            }
+
+        });
 
         $scope.selectArtist = function(item){
-          $scope.selectedArtist = item;
-          $state.go('main.list.artist', {artist: item.name});
+            $scope.selectedArtist = item;
+            $state.go('main.list.artist', {artist: item.name});
         };
-
-        $scope.initArtist = function(name){
-            if($scope.items && name){
-              $scope.selectedArtist = _.find($scope.items,
-                                           function(item){
-                                                return item.name === name;
-                                              });
-            }
-          };
-      }])
-    .controller('ArtistCtrl', ['$scope', '$http', '$state', '$stateParams', function($scope, $http, $state, $stateParams){
-        $scope.initStart($stateParams.start);
-        $scope.$watch('selectedArtist', function(oldv, newv){
-            var val = newv || oldv;
-            if(val){
-              $http.get(val.detail_url).then(function(results){
-                    $scope.selectedArtistAlbums = results.data.items;
-                  });
-            }
-          });
-
-        $scope.$watch('items', function(){
-            $scope.initArtist($stateParams.artist);
-          });
-      }]);
+    }])
+    .controller('ArtistCtrl', ['$scope', 'MusicService', '$state', '$stateParams', function($scope, MusicService, $state){
+        MusicService.getArtistDetail($state.params.artist).then(function(results){
+            $scope.selectedArtistAlbums = results.data.items;
+        });
+    }]);
